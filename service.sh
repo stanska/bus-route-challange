4d3ca54b5fe5ef73e5757b70cf28b9b1daf68353
stanka dalekova <stanska@gmail.com>
2017-04-14 17:48:42 +0300

    add log file

--- service.sh
+++ service.sh
@@ -1,12 +1,13 @@
+LOGFILE=/tmp/bus-route-challenge.log
 case "$1" in
         start)
-            BUS_SERVICE_PATH=$2;java -jar ./target/bus-route-challenge-1.0-SNAPSHOT-fat.jar  -conf ./src/main/conf/bus-route-challenge-conf.json&
+            BUS_SERVICE_PATH=$2;java -jar ./target/bus-route-challenge-1.0-SNAPSHOT-fat.jar  -conf ./src/main/conf/bus-route-challenge-conf.json >> $LOGFILE  2>&1 &
             ;;
         stop)
             ps -ef | grep "java -jar ./target/bus-route-challenge-1.0-SNAPSHOT-fat.jar" | grep -v grep | awk '{print $2}' | xargs kill
             ;;
         *)
-            echo $"Usage: $0 {start|stop} FILE"
+            echo $"Usage: $0 {start FILE|stop}"
             exit 1
 
 esac
